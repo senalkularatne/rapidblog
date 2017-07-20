0c90b3d718a607e91619a7df228044b44a0cf7de
Senal Kularatne <senalk90@gmail.com>
2017-07-20 01:48:54 -0400

    Finish show page

--- app.js
+++ app.js
@@ -83,6 +83,18 @@ app.post("/blogs", function(req, res){
 	});
 });
 
+// SHOW Route
+app.get("/blogs/:id", function(req, res){
+	
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		} else {
+			res.render("show", {blog: foundBlog});
+		}
+	})
+});
+
 
 
 
